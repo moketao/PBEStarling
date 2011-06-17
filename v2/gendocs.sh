596b8d2c0b9a2c9c275e8d546bf663086c9bd478
Ben Garney <ben.garney@gmail.com>
2011-06-17 14:49:24 -0700

    Added docco docs for PBE2.

---
+++ v2/gendocs.sh
@@ -0,0 +1,2 @@
+date
+/Users/beng/research/docco.as3/bin/docco /Users/beng/projects/pushbuttonengine.git/src/*.as /Users/beng/projects/pushbuttonengine.git/src/demos/*.as /Users/beng/projects/pushbuttonengine.git/src/demos/*/*.as
