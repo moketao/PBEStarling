43ec51f850d08399e29f2d6f5db596dda6e8491d
Ben Garney <ben.garney@gmail.com>
2011-06-23 16:39:22 -0700

    Updating docs.

--- v2/gendocs.sh
+++ v2/gendocs.sh
@@ -1,3 +1,3 @@
 date
 /Users/beng/research/docco.as3/bin/docco \
-    `find /Users/beng/projects/pushbuttonengine.git/src/* | grep \\.as | grep -v greensock`
+    `find /Users/beng/projects/pushbuttonengine.git/* | grep \\.as | grep -v greensock`
